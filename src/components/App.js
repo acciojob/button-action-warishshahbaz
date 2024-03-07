d65a7c30de3b235ccf8e271ff8520080ae49c4ae
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-03-07 17:29:17 +0530

    implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = (props) => {
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={`${toggle ? "show" : "hide"}`}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+      <button id="click" onClick={() => setToggle(!toggle)}>
+        {toggle ? "show" : "hide"}
+      </button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
